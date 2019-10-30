d93970819bc4ebb458128d4c8e9e0ec611c63971
Asaolu Elijah <Asaoluelijah7@gmail.com>
2019-10-30 15:18:49 +0100

    Required phone number in adding trustee

--- php/add_trustee.php
+++ php/add_trustee.php
@@ -2,8 +2,9 @@
     require("app.php");
     $trusteeEmail = $_REQUEST['trustee_email'];
     $userId = $_REQUEST['user_id'];
+    $phone = $_REQUEST['phone'];
 
     $app = new App;
-    $result = $app->addTrustee($trusteeEmail,$userId);
+    $result = $app->addTrustee($trusteeEmail, $userId, $phone);
     echo $result;
 ?>
